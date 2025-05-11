7264d999e38593b2ea06f35322c989a8a37b286f
kambiev.b.m@gmail.com <kambiev.b.m@gmail.com>
2025-05-11 21:59:19 +0300

    Entities

---
+++ src/Recipes.Domain/Measure.cs
@@ -0,0 +1,9 @@
+﻿namespace Recipes.Domain
+{
+    public class Measure
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+    }
+}
---
+++ src/Recipes.Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Recipes.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
